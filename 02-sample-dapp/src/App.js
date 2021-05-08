fb9845b776344c04ccc716c53466c6d24d4f4662
Diego Dorado <diegodorado@gmail.com>
2021-05-08 13:45:43 -0300

    Add reset style and query contract state

--- 02-sample-dapp/src/App.js
+++ 02-sample-dapp/src/App.js
@@ -1,28 +1,74 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
-import {getWeb3, getAccount, getBalance} from './eth/network'
+import {getDeployedContract, getAccount, requestAccount} from './eth/network'
+
+const CONTRACT_ADDRESS = '0x1D2561D18dD2fc204CcC8831026d28375065ed53'
+const Counter = ({account}) => {
+
+  const [contract, setContract ] = useState(null)
+  const [value, setValue ] = useState(null)
+
+  const fetchContractAsync = async () => {
+    const contract = await getDeployedContract(account)
+    setContract(contract)
+    const value = await contract.methods.value().call()
+    setValue(value)
+  }
+
+  useEffect( () => {
+    fetchContractAsync()
+  },[]) // run the effect only once
+
+  const onButtonClick = () => {
+    contract.methods.increase().send()
+  }
+
+  return (
+          <>
+            <h2>Connected!</h2>
+            <h2>Wallet Address: {account}</h2>
+            <h2>Contract value: {value ? value : 'Loading...'}</h2>
+            <button onClick={onButtonClick}>Increase</button>
+          </>
+  )
+}
+
 
 const App = () => {
 
   // destructuring assignment
+  const [loading, setLoading] = useState(true)
   const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(null)
 
-  const onConnectWalletPress = async () => {
+  const fetchAccountAsync = async () => {
     const account = await getAccount()
-    const balance = await getBalance(account)
     setAccount(account)
-    setBalance(balance)
+    setLoading(false)
+  }
+
+  useEffect( () => {
+    fetchAccountAsync()
+  },[]) // run the effect only once
+  
+  const onConnectWalletPress = async () => {
+    try {
+      const account = await requestAccount()
+      setAccount(account)
+    } catch (e) {
+      /* handle error */
+    }
   }
 
   return (
     <div className="App">
-      <h1>COUNTER</h1>
-      <h2>Wallet Address: {account}</h2>
-      <h2>Balance: {balance}</h2>
-      <button onClick={onConnectWalletPress}>Connect Wallet</button>
+      <h1>Counter DApp</h1>
+      {loading ? <h2>Loading...</h2>
+        : !account 
+        ? <button onClick={onConnectWalletPress}>Connect Wallet</button>
+        : <Counter {...{account}} />
+      }
     </div>
-  );
+  )
 }
 
 export default App;
--- 02-sample-dapp/src/eth/network.js
+++ 02-sample-dapp/src/eth/network.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
-//import Artifacts from '../contracts/Artifacts.json'
+import Artifacts from '../contracts/Artifacts.json'
 
 let _web3
 const getWeb3 = () => {
@@ -12,7 +12,13 @@ const getWeb3 = () => {
 const getAccount = async () =>{
   const web3 = getWeb3();
   const accounts = await web3.eth.getAccounts()
-  return accounts[0]
+  return accounts.length > 0  ?  accounts[0] : null
+}
+
+const requestAccount = async () =>{
+  const web3 = getWeb3();
+  const accounts = await web3.eth.requestAccounts()
+  return accounts.length > 0  ?  accounts[0] : null
 }
 
 const getBalance = async (address) =>{
@@ -21,25 +27,17 @@ const getBalance = async (address) =>{
   return balance
 }
 
-export {getWeb3, getAccount, getBalance}
 
-/*
 const Counter = ( web3, address, options = {} ) => {
   const name = "contracts/Counter.sol:Counter"
   const {abi} = Artifacts.contracts[name]
   return new web3.eth.Contract(abi, address, options )
 }
 
-const getDeployed = async () => {
+const getDeployedContract = async (from) => {
  const web3 = getWeb3();
- const data = await web3.eth.requestAccounts()
-
- const accounts = await web3.eth.getAccounts()
- const from = accounts[0]
- console.log(from,web3.eth.defaultAccount)
-
  const addr = "0x1D2561D18dD2fc204CcC8831026d28375065ed53";
  return Counter(web3, addr, { from });
 }
 
-*/
+export {getWeb3, getAccount, requestAccount, getBalance, getDeployedContract}
